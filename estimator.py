135c67cc6840512c8f2596fbdfb08ec295fdfe50
Mehdi Sebbar <mehdi.sebbar.0@gmail.com>
2017-02-23 20:32:36 +0100

    correct

---
+++ estimator.py
@@ -0,0 +1,13 @@
+import operator
+
+def estimator(data):
+    res = {}
+    for endpoint_id in data.keys():
+        if data[endpoint_id]["caches"] != {}:
+            most_viewed_video_id = max(data[endpoint_id]["requests"].iteritems(), key=operator.itemgetter(1))[0]
+            best_cache = min(data[endpoint_id]["caches"].iteritems(), key=operator.itemgetter(1))[0]
+            temp_lst = res.get(best_cache,[])
+            if most_viewed_video_id not in temp_lst:
+	            temp_lst.append(most_viewed_video_id)
+            res[best_cache]=temp_lst
+    return res
