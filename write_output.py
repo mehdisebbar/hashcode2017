71ba6e2a5a988a5e33a32cd855c0294246faa837
Francois Aubert <cauchy@cauchy-2.local>
2017-02-23 20:03:53 +0100

    file name ok

--- write_output.py
+++ write_output.py
@@ -3,9 +3,9 @@
 # result_dict = {0:[1,2],1:[2,3]}
 
 
-def write_output(result_dict):
+def write_output(result_dict, file_name):
     nb_cache_server = len(result_dict.keys())
-    with open('output', 'w+') as infile:
+    with open(file_name, 'w+') as infile:
         infile.write(str(nb_cache_server) + '\n')
         for key, values in result_dict.iteritems():
             values_to_string = [str(value) for value in values]
