c4f40f9b078918015f5b5922f7d68d0f9f2a7cd5
Mehdi Sebbar <mehdi.sebbar.0@gmail.com>
2017-02-23 20:19:02 +0100

    ttooto

--- estimator.py
+++ estimator.py
@@ -7,6 +7,7 @@ def estimator(data):
             most_viewed_video_id = max(data[endpoint_id]["requests"].iteritems(), key=operator.itemgetter(1))[0]
             best_cache = min(data[endpoint_id]["caches"].iteritems(), key=operator.itemgetter(1))[0]
             temp_lst = res.get(best_cache,[])
-            temp_lst.append(most_viewed_video_id)
+            if most_viewed_video_id not in temp_lst:
+	            temp_lst.append(most_viewed_video_id)
             res[best_cache]=temp_lst
     return res
